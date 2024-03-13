260058a30d6be2305e75c740c2aa3be9a6ed39ca
Soner <sonersg@outlook.com>
2024-03-13 22:57:27 +0300

    thresholding

---
+++ img-processing/thresholding.m
@@ -0,0 +1,25 @@
+%% Thresholding
+
+clc, clear all, close all, format compact
+
+pkg load image
+
+img = imread("desktop.png");
+
+img_raw = img;
+
+gry_img = rgb2gray(img);
+
+##THRESHOLDING
+##threshold = 0.8;
+threshold = graythresh(gry_img);
+BW = im2bw(gry_img, threshold); % imbinarize
+figure, imshowpair(gry_img, BW, "montage");
+
+##DYNAMIC TITLE
+title_str = strcat("Threshold set to: ", num2str(threshold));
+figure, imshow(BW), pause(0.1);
+title(title_str);
+
+
+
